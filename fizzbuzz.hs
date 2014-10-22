152dfa8c6be30431b6be528f901f8ade1acb2145
SimonLab <SimonLab@users.noreply.github.com>
2014-10-22 16:55:36 +0100

    Simple FizzBuzz in haskell

---
+++ fizzbuzz.hs
@@ -0,0 +1,17 @@
+-- Simple FizzBuzz
+import Data.List
+fizzBuzz :: [Int] -> String
+fizzBuzz [] = []
+fizzBuzz (x:xs)   | mod x 15 == 0 = "FizzBuzz" ++ fizzBuzz xs
+                  | mod x 3 == 0 = "Fizz" ++ fizzBuzz xs
+                  | mod x 5 == 0 = "Buzz" ++ fizzBuzz xs
+                  | otherwise = show x ++ fizzBuzz xs
+
+
+fizzBuzz' :: [Int] -> String
+fizzBuzz' = intercalate "," . map toFizzBuzz
+toFizzBuzz :: Int -> String
+toFizzBuzz i | mod i 15 == 0 = "FizzBuzz"
+       | mod i 3 == 0 = "Fizz"
+       | mod i 5 == 0 = "Buzz"
+       | otherwise = show i
